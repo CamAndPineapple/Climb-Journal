6c999f7403f959f7d8f401bf2bb307b865adb804
Camden Rudisill <camden.rudisill@richmond.edu>
2016-01-10 00:04:38 -0500

    Add: ClimbView delete button removes view and destroys model

--- app/js/views/climb.js
+++ app/js/views/climb.js
@@ -9,6 +9,17 @@ var ClimbView = Backbone.View.extend({
   className: "climbContainer",
   template: _.template( $('#climbTemplate').html() ),
 
+  events: {
+      'click .delete': 'deleteClimb'
+  },
+
+  deleteClimb: function() {
+    // Destroy model and remove view from page
+    this.model.destroy
+    this.remove();
+
+  },
+
 
   initialize: function() {
     this.listenTo(this.model, "change", this.render);
